d40de3125ccf195a1c05f7cf8d40410648728869
leaisrevolution <leaisrevolution@gmail.com>
2022-11-22 16:25:41 +0900

    [Add] 짝수 홀수 개수

---
+++ Lea/프로그래머스_코테입문/Python/20221122_프로그래머스_짝수 홀수 개수.py
@@ -0,0 +1,30 @@
+
+
+# 짝수 홀수 개수
+
+# 정수가 담긴 리스트 num_list가 주어질 때,
+#  num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+
+num_list = [1,2,3,4,5,6,7,8]
+
+def solution(num_list):
+    even_number = []
+    odd_number = []
+
+    answer = []
+
+    for i in num_list:
+        if i % 2 == 0:
+            even_number.append(i)
+        else:
+            odd_number.append(i)
+
+    len_even = len(even_number)
+    len_odd = len(odd_number)
+
+    answer.append(len_even)
+    answer.append(len_odd)
+
+    return answer
+
