58557f7978f1a03750d7987fd39e6b3e42d35c8a
leaisrevolution <leaisrevolution@gmail.com>
2022-11-22 16:31:55 +0900

    [Add] 점의 위치 구하기

---
+++ Lea/프로그래머스_코테입문/Python/20221122_프로그래머스_점의 위치 구하기.py
@@ -0,0 +1,23 @@
+
+
+# 점의 위치 구하기
+
+## 사분면은 한 평면을 x축과 y축을 기준으로 나눈 네 부분입니다. 사분면은 아래와 같이 1부터 4까지 번호를매깁니다.
+
+
+dot = [0, 1]
+
+def solution(dot):
+    answer = 0
+    if dot[0] > 0 and dot[1] > 0:
+        answer = 1
+    elif dot[0] < 0 and dot[1] > 0:
+        answer = 2
+    elif dot[0] < 0 and dot[1] < 0:
+        answer = 3
+    else :
+        answer = 4
+
+    return answer
+
+
